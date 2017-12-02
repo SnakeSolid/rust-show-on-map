5119d01f629e95541fddf29a4b2b79858fb60e73
Shabanov Anton <snakesolid@ngs.ru>
2017-12-02 10:05:14 +0300

    frontend connection settings form

--- public/static/js/appModel.js
+++ public/static/js/appModel.js
@@ -4,6 +4,37 @@ define([ "knockout", "reqwest", "openLayers", "mapModel" ], function(ko, reqwest
 	return function() {
 		const self = this;
 
+		this.isConnectionVisible = ko.observable(false);
+		this.isShowPlacesVisible = ko.observable(false);
+		this.isShowRoadsVisible = ko.observable(false);
+		this.isClearShapesVisible = ko.observable(false);
+		this.isShowPlacesEnabled = ko.observable(false);
+		this.isShowRoadsEnabled = ko.observable(false);
+		this.isClearShapesEnabled = ko.observable(false);
 		this.map = map.create();
+
+		this.isShowPlacesDisabled = ko.pureComputed(function() {
+			return !this.isShowPlacesEnabled();
+		}, this);
+
+		this.isShowRoadsDisabled = ko.pureComputed(function() {
+			return !this.isShowRoadsEnabled();
+		}, this);
+
+		this.isClearShapesDisabled = ko.pureComputed(function() {
+			return !this.isClearShapesEnabled();
+		}, this);
+
+		this.showConnection = function() {
+			const oldState = self.isConnectionVisible();
+
+			self.isShowPlacesVisible(false);
+			self.isShowRoadsVisible(false);
+			self.isConnectionVisible(!oldState);
+		};
+
+		this.saveConnection = function() {
+			console.log("saved");
+		};
 	};
 });
