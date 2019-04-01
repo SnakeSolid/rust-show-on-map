899145ab9a3c586d4ff11a7e1e24df8dcd0e3d75
Shabanov Anton <snakesolid@ngs.ru>
2019-04-01 21:13:53 +0300

    updated dependencies

--- public/static/js/init.js
+++ public/static/js/init.js
@@ -3,10 +3,10 @@
 requirejs.config({
 	baseUrl: "/static/js",
 	paths: {
-		knockout: ["https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.2/knockout-min", "lib/knockout-min"],
-		openLayers: ["https://cdnjs.cloudflare.com/ajax/libs/openlayers/4.5.0/ol", "lib/ol"],
+		knockout: ["https://cdnjs.cloudflare.com/ajax/libs/knockout/3.5.0/knockout-min", "lib/knockout-min"],
+		openLayers: ["https://cdnjs.cloudflare.com/ajax/libs/openlayers/4.6.5/ol", "lib/ol"],
 		reqwest: ["https://cdnjs.cloudflare.com/ajax/libs/reqwest/2.0.5/reqwest.min", "lib/reqwest.min"],
-		semantic: ["https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/semantic.min", "lib/semantic.min"],
+		semantic: ["https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min", "lib/semantic.min"],
 		text: ["https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min", "lib/text.min"],
 	},
 	shim: {
